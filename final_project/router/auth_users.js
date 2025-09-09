23366fd9ee060bf5732142fe9ceba01964385661
john-uriarte <uriarte.johnlennon@gmail.com>
2025-09-09 09:30:20 -0400

    Add t8-t9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,6 +5,24 @@ const regd_users = express.Router();
 
 let users = [];
 
+// Middleware to authenticate users using JWT
+regd_users.use("/auth", function auth(req, res, next) {
+  if (req.session.authorization) { // Get the authorization object stored in the session
+    token = req.session.authorization['accessToken']; // Retrieve the token from authorization object
+    jwt.verify(token, "access", (err, user) => { // Use JWT to verify token
+      if (!err) {
+        req.user = user;
+        req.username = req.session.authorization['username'];
+        next();
+      } else {
+        return res.status(403).json({ message: "User not authenticated" });
+      }
+    });
+  } else {
+    return res.status(403).json({ message: "User not logged in" });
+  }
+});
+
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
   let userswithsamename = users.filter((user) => {
@@ -48,7 +66,32 @@ regd_users.post("/login", (req,res) => {
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.username;
+  const isbn = req.params.isbn;
+  const review = req.body.review;
+
+  if(books.hasOwnProperty(isbn)) {
+    books[isbn].reviews[username] = review;
+  } else {
+    res.send("ISBN not found!"); 
+  }
+
+  return res.status(200).json({message: "Review from: " + username + " added!"});
+});
+
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  //Write your code here
+  const username = req.username;
+  const isbn = req.params.isbn;
+
+  if(books.hasOwnProperty(isbn)) {
+    delete books[isbn].reviews[username];
+  } else {
+    res.send("ISBN not found!"); 
+  }
+
+  return res.status(200).json({message: "Review from: " + username + " deleted!"});
 });
 
 module.exports.authenticated = regd_users;
