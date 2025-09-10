bf776a9e66d5a4a3d7f44e8424b93920030868e5
john-uriarte <uriarte.johnlennon@gmail.com>
2025-09-10 07:48:28 -0400

    Add t9 - t14

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,7 +24,15 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.send(books);
+
+  let myPromise = new Promise((resolve,reject) => {
+    setTimeout(() => {
+      resolve(books)
+    }, 500)});
+
+  myPromise.then((data) => {
+    return res.send(data);
+  })  
 });
 
 // Get book details based on ISBN
@@ -32,32 +40,48 @@ public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
 
-  if(books.hasOwnProperty(isbn)) {
-    res.send(books[isbn]); 
-  } else {
-    res.send("ISBN not found!"); 
-  }
-   
- });
+  let myPromise = new Promise((resolve,reject) => {
+    if(books.hasOwnProperty(isbn)) {
+        resolve(books[isbn]); 
+    } else {
+        reject("ISBN not found!"); 
+    }  
+  });
+
+  myPromise.then((data) => {
+    return res.send(data);
+  },
+  (err) => {
+    return res.send(err);
+  });
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
   const author = req.params.author;
 
-  let arr = [];
-  for (const key in books) {
-    let book = books[key];
-    if (book.author == author) {
-        arr.push(book);
+  let myPromise = new Promise((resolve,reject) => {
+    let arr = [];
+    for (const key in books) {
+      let book = books[key];
+      if (book.author == author) {
+          arr.push(book);
+      }
     }
-  }
-  if (arr.length > 0) {
-    res.send(arr);
-  } else {
-    res.send("Author not found");  
-  }
-  
+    if (arr.length > 0) {
+      resolve(arr);
+    } else {
+      reject("Author not found");  
+    }
+  });
+
+  myPromise.then((data) => {
+    return res.send(data);
+  },
+  (err) => {
+    return res.send(err);
+  });
 });
 
 // Get all books based on title
@@ -65,18 +89,27 @@ public_users.get('/title/:title',function (req, res) {
   //Write your code here
   const title = req.params.title;
 
-  let arr = [];
-  for (const key in books) {
-    let book = books[key];
-    if (book.title == title) {
-        arr.push(book);
+  let myPromise = new Promise((resolve,reject) => {
+    let arr = [];
+    for (const key in books) {
+      let book = books[key];
+      if (book.title == title) {
+          arr.push(book);
+      }
     }
-  }
-  if (arr.length > 0) {
-    res.send(arr);
-  } else {
-    res.send("Title not found");  
-  }
+    if (arr.length > 0) {
+      resolve(arr);
+    } else {
+      reject("Title not found");  
+    }
+  });
+
+  myPromise.then((data) => {
+    return res.send(data);
+  },
+  (err) => {
+    return res.send(err);
+  });
 });
 
 //  Get book review
